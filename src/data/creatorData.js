3b6138c9e9139a3f1ad2235044f82461d0897efa
Satvik Chachra <satvikchachra19@gmail.com>
2020-12-16 03:11:46 +0530

    feat: Added creators data

---
+++ src/data/creatorData.js
@@ -0,0 +1,94 @@
+import comedyPic1 from '../assets/creators_dp/comedy/tanmaybhat.jpeg';
+import comedyPic2 from '../assets/creators_dp/comedy/kennysebastian.jpg';
+import comedyPic3 from '../assets/creators_dp/comedy/abhishekupmanyu.jpg';
+
+import techPic1 from '../assets/creators_dp/tech/tanaypratap.jpg';
+import techPic2 from '../assets/creators_dp/tech/gauravsen.jpg';
+import techPic3 from '../assets/creators_dp/tech/takeuforward.jpg';
+
+import brainyPic1 from '../assets/creators_dp/brainy/varunmayya.jpg';
+import brainyPic2 from '../assets/creators_dp/brainy/lexfridman.jpg';
+import brainyPic3 from '../assets/creators_dp/brainy/beerbiceps.jpg';
+
+export const comedyChannels = [
+    {
+        id: c1,
+        name: 'Tanmay Bhat',
+        description: 'That AIB guy',
+        link: 'https://www.youtube.com/user/tanmay316',
+        pic: comedyPic1
+    },
+
+    {
+        id: c2,
+        name: 'Kenny Sebastian',
+        description: 'Stand Up Comedian, Musician and Filmmaker.',
+        link: 'https://www.youtube.com/user/kennethseb',
+        pic: comedyPic2
+    },
+
+    {
+        id: c3,
+        name: 'Abhishek Upmanyu',
+        description: 'Stand Up Comedian',
+        link: 'https://www.youtube.com/user/upmanyukaran',
+        pic: comedyPic3
+    }
+];
+
+
+export const techChannels = [
+    {
+        id: t1,
+        name: 'Tanay Pratap',
+        description: 'Mentor. Engineer at Microsoft',
+        link: 'https://www.youtube.com/c/TanayPratap',
+        pic: techPic1
+    },
+
+    {
+        id: t2,
+        name: 'Gaurav Sen',
+        description: 'Educational content creator. Software Engineer.',
+        link: 'https://www.youtube.com/c/GauravSensei',
+        pic: techPic2
+    },
+
+    {
+        id: t3,
+        name: 'take u Forward',
+        description: 'Educational content creator. Engineer at Media.net',
+        link: 'https://www.youtube.com/channel/UCJskGeByzRRSvmOyZOz61ig',
+        pic: techPic3
+    }
+];
+
+
+export const brainyChannels = [
+    {
+        id: b1,
+        name: 'Varun Mayya',
+        description: 'Leader of the AvalonArmy',
+        link: 'https://www.youtube.com/c/VarunMayya',
+        pic: brainyPic1
+    },
+
+    {
+        id: b2,
+        name: 'Lex Fridman',
+        description: 'AI researcher. Podcast host.',
+        link: 'https://www.youtube.com/c/lexfridman',
+        pic: brainyPic2
+    },
+
+    {
+        id: b3,
+        name: 'BeerBiceps',
+        description: 'Social Media Entrepreneur',
+        link: 'https://www.youtube.com/c/BeerBicepsOfficial/',
+        pic: brainyPic3
+    }
+];
+
+
+
